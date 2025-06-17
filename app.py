035f185a46ca486a1c756aad6313c684a3e79ba0
Isabele Vitoria Agostini <isabele.agostini@MLTBR.LOCAL>
2025-06-17 13:44:35 -0300

    add Flask

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from datetime import datetime
+import os
 
 app = Flask(__name__)
 
@@ -11,4 +12,5 @@ def receber_dados():
     return {'status': 'ok'}
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
